5e2f043193e4a1df1e768438f1b2a6cc5a245b57
Raymond Samuray007 <31334367+CrazyAsianBoy@users.noreply.github.com>
2023-10-06 15:17:27 +0800

    add nav in index.html and img dir with images file for index.html

---
+++ js/room1.js
@@ -0,0 +1,11 @@
+const panoContainer = document.getElementById("panorama")
+
+const panorama = new PANOLENS.ImagePanorama( './equirectengular/iStock-1.jpg' );
+const viewer = new PANOLENS.Viewer({
+    container:panoContainer,
+    autoRotate:true,
+    autoRotateSpeed:0.3,
+    controlBar:true,
+    DeviceOrientationControls: true
+});
+viewer.add( panorama );
